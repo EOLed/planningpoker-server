0856093360ad8e771bb9e067990f373bb5542a8f
Amos Chan <amosschan@gmail.com>
2013-07-14 21:08:16 -0400

    Updates jshint options and default grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+/*jslint node: true*/
 'use strict';
 
 var path = require('path');
@@ -6,11 +7,10 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['api/**/*.js', 'config/*.js', '*.js']
+      all: ['*.js', 'routes/*.js', 'stores/*.js', 'test/**/*.js']
     },
     jasmine_node: {
-      projectRoot: '.',
-      specFolders: ['test/spec']
+      projectRoot: '.'
     },
     express: {
       custom: {
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-jasmine-node');
   grunt.loadNpmTasks('grunt-express');
 
-//  grunt.registerTask('default', ['jshint', 'jasmine_node']);
+  grunt.registerTask('default', ['jshint', 'jasmine_node']);
   grunt.registerTask('test', ['jasmine_node']);
   grunt.registerTask('s', ['express', 'express-keepalive']);
 };
